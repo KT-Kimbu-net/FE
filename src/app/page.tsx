6dbdf6965c78439aec24ee8373909c7a6cdbd2d2
hyejeong <vpdrnls566@gmail.com>
2024-07-09 13:35:15 +0900

    ✨feat #6 : 퀴즈 모달 구현
    - 7개 모달창 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
+"use client";
 import Header from "@/components/header/Header";
+import Test from "@/components/Test";
 import Image from "next/image";
+import { useModalState } from "@/store/modal";
 
 export default function page() {
+  const { setModalName } = useModalState();
   return (
     <>
       <main>
-        <div className="relative w-full" style={{ height: "800px" }}>
-          <Image
+        {/* 퀴즈 플로팅 버튼 임시 */}
+        <div className="z-40 fixed top-40 right-10">
+          <button
+            onClick={() => {
+              setModalName("quizStart");
+            }}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg text-3xl"
+          >
+            Quiz
+          </button>
+        </div>
+        <div className="relative w-full h-800">
+          <img
             src="/2024_season_bg_web.png"
-            style={{ objectFit: "cover", objectPosition: "center" }}
-            fill
-            alt={""}
+            className="object-cover object-center w-full h-full"
+            alt=""
           />
         </div>
       </main>
