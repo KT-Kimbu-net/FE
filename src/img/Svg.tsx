15af539a0e32d9e8e8f41ca0052994641b1ed958
hyejeong <vpdrnls566@gmail.com>
2024-07-11 15:14:45 +0900

    ✨feat : 퀴즈에 랭킹 추가
    - flow #11

---
+++ src/img/Svg.tsx
@@ -0,0 +1,34 @@
+export const SvgQuizProblemLoading = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100"
+      height="100"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeDasharray="15"
+        strokeDashoffset="15"
+        strokeLinecap="round"
+        strokeWidth="2"
+        d="M12 3C16.9706 3 21 7.02944 21 12"
+      >
+        <animate
+          fill="freeze"
+          attributeName="stroke-dashoffset"
+          dur="0.3s"
+          values="15;0"
+        />
+        <animateTransform
+          attributeName="transform"
+          dur="1.5s"
+          repeatCount="indefinite"
+          type="rotate"
+          values="0 12 12;360 12 12"
+        />
+      </path>
+    </svg>
+  );
+};
